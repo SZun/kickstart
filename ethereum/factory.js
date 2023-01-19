4178da09f790fa7ea07e3a4f7725e94b120714c8
Sam Zun <zunsamuel97@gmail.com>
2023-01-18 21:30:08 -0600

    completed front-end of dApp

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  '0x90e2b94532734E2999D4919Ea1b5263Bf35d4B0d'
+  CampaignFactory.abi,
+  "0xD96a63CaAEC9a3742acA258F7165F210ac4DC80b"
 );
 
 export default instance;
